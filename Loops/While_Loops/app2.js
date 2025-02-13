0b02809a0e20eaec77db4d57a3f77ea65df7609e
hbshin <hbshin9036@gmail.com>
2025-02-13 16:50:18 +0900

    commit

--- Loops/While_Loops/app2.js
+++ Loops/While_Loops/app2.js
@@ -1,4 +1,3 @@
-let maximum = parseInt(prompt("Enter the maximum number!"));
 
 /*
     Math.random() 정적 메서드는 0보다 크거나 1보다 
@@ -11,10 +10,39 @@ let maximum = parseInt(prompt("Enter the maximum number!"));
     floor 함수 : Math.floor() 정적 메서드는 언제나 버림 처리하고 
     주어진 숫자와 같거나 작은 정수 중에서 가장 큰 수를 반환합니다.
 */
+
+
+// while문을 통해서 사용자가 반드시 유효한 숫자를 입력하도록 
+let maximum = parseInt(prompt("Enter the maximum number!"));
 while (!maximum){
     maximum = parseInt(prompt("Enter a vaild number!"));
 }
-
 const targetNum = Math.floor(Math.random() * maximum) + 1;
 console.log(targetNum);
 
+
+// 사용자가 첫번째 시도에서 'q' 를 입력해서 게임을 종료할 수 있다.
+let guess = prompt("Enter your first guess! (Type 'q' to quit) ");
+let attempts = 1;
+
+while(parseInt(guess) !== targetNum){
+    // guess 값이 'q'인지 확인
+    if(guess === 'q') break;
+    // 'q' 가 아니면 정수로 변환
+    guess = parseInt(guess);
+    if(guess > targetNum){
+        guess = prompt("Too high! Enter a new guess:");
+        attempts++;
+    }else if(guesss < targetNum){
+        guess = prompt("Too low! Enter a new guess:");
+        attempts++;
+    }else{
+        guess = prompt("Invalid guess. Please enter a number or 'q' to quit")
+    }
+}
+if(guess === 'q'){
+    console.log("OK YOU QUIT!")
+}else{
+    console.log("CONGRATS YOU WIN!");
+    console.log(`You got it! It took you ${attempts} guesses`);
+}
